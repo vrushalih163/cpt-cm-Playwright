233bcf4af6c69be35a4142531a6cb8fcd2c99f19
vrushalih163 <vrushali.honnatti@wellsky.com>
2024-08-20 17:47:25 +0530

    Updated xpath and environment files

--- pages/Transition_Pages/TransitionContextNavigator.js
+++ pages/Transition_Pages/TransitionContextNavigator.js
@@ -3,13 +3,13 @@ import { expect } from '@playwright/test';
 export class TransitionContextNavigator{
     constructor(page){
         this.page = page;
-        this.tabProviders = page.locator('xpath=//div[@class="mat-tab-labels"]//div[@class="mat-tab-label-content" and contains(.,"Providers")]');
-        this.tabSharedChoice = page.locator('xpath=//div[@class="mat-tab-labels"]//div[@class="mat-tab-label-content" and contains(.,"Shared Choice")]');
-        this.tabAttachments = page.locator('xpath=//div[@class="mat-tab-labels"]//div[@class="mat-tab-label-content" and contains(.,"Attachments")]');
-        this.tabClinical = page.locator('xpath=//div[@class="mat-tab-labels"]//div[@class="mat-tab-label-content" and contains(.,"Clinical")]');
-        this.tabForms = page.locator('xpath=//div[@class="mat-tab-labels"]//div[@class="mat-tab-label-content" and contains(.,"Forms")]');
-        this.tabInformation = page.locator('xpath=//div[@class="mat-tab-labels"]//div[@class="mat-tab-label-content" and contains(.,"Information")]');
-        this.tabSend = page.locator('xpath=//div[@class="mat-tab-labels"]//div[@class="mat-tab-label-content" and contains(.,"Send")]');
+        this.tabProviders = page.locator('xpath=//div[contains(@class,"mat-tab-labels")]//div[@class="mat-tab-label-content" and contains(.,"Providers")]');
+        this.tabSharedChoice = page.locator('xpath=//div[contains(@class,"mat-tab-labels")]//div[@class="mat-tab-label-content" and contains(.,"Shared Choice")]');
+        this.tabAttachments = page.locator('xpath=//div[contains(@class,"mat-tab-labels")]//div[@class="mat-tab-label-content" and contains(.,"Attachments")]');
+        this.tabClinical = page.locator('xpath=//div[contains(@class,"mat-tab-labels")]//div[@class="mat-tab-label-content" and contains(.,"Clinical")]');
+        this.tabForms = page.locator('xpath=//div[contains(@class,"mat-tab-labels")]//div[@class="mat-tab-label-content" and contains(.,"Forms")]');
+        this.tabInformation = page.locator('xpath=//div[contains(@class,"mat-tab-labels")]//div[@class="mat-tab-label-content" and contains(.,"Information")]');
+        this.tabSend = page.locator('xpath=//div[contains(@class,"mat-tab-labels")]//div[@class="mat-tab-label-content" and contains(.,"Send")]');
         this.lnkManageReferralBreadCrumb = page.locator('xpath=//span[contains(@class,"breadcrumb-item")]//a[@id="anchorLabelClick1"]');
         this.iconManageReferral = page.locator('xpath=//div[@id="infoIconDiv"]//mat-icon[@id="iconDashboardCustomize"]');
         this.iconProviderSearch = page.locator('xpath=//mat-icon[@id="iconSearch"]/parent::div[@class="col-auto ng-star-inserted"]');
