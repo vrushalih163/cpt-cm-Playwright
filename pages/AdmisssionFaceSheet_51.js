4b2d16b5eec88db01f99e60abd9c82ec66d09560
MIS\Micho.Eshete <Micho.Eshete@careporthealth.com>
2024-10-18 09:18:14 -0400

    Tasking - Admission Task (Assigned, Pastdue and Completed), Notifications by Pager and Email

--- pages/AdmisssionFaceSheet_51.js
+++ pages/AdmisssionFaceSheet_51.js
@@ -1,9 +1,11 @@
 //Autor: Rajakumar Maste, Created Date: 16 Aug 2024
+import { page, expect } from '@playwright/test';
 
 export class AdmissionFaceSheet{
     constructor(page){
         this.page = page;
         this.TransitionDignosis_Link = page.getByRole('link', { name: 'Transition Diagnostics' });
+        this.taskDetails_Link = page.getByRole('link', { name: 'Task Details' });
     }
 
     /**
@@ -13,5 +15,13 @@ export class AdmissionFaceSheet{
         await this.TransitionDignosis_Link.click();
         return this.page;
     }
+
+    /**
+     * Click on Task Details link
+     */
+    async TaskDetailsLinkClick(){
+        await this.taskDetails_Link.click();
+        return this.page;
+    }    
 
 }
